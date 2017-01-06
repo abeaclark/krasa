068d480b8ff0264d37d9f29d4a12dfb5f1b182a8
abe_clark <abeaclark@gmail.com>
2017-01-06 15:40:33 -0800

    adjust tags

--- html.js
+++ html.js
@@ -26,10 +26,11 @@ module.exports = React.createClass({
             content="width=device-width, initial-scale=1.0"
           />
           <meta name="og:description" content="Krasa is a San Francisco based digital Design and Development company. We specialize in designing and building fast, reliable, and sexy mobile apps, websites, and other digital experiences." />
-          <meta name="og:type" content="website"/>
-          <meta name="og:image" content="https://www.krasadev.com/static/Abe_Clark_375x300.9cdeecbe.jpeg"/>
-          <meta name="og:url" content="www.krasadev.com"/>
-          <meta name="og:og:app_id" content="1871871486381228"/>
+          <meta name="og:type" content="website" />
+          <meta name="og:image" content="https://www.krasadev.com/static/Abe_Clark_375x300.9cdeecbe.jpeg" />
+          <meta name="og:url" content="www.krasadev.com" />
+          <meta name="og:app_id" content="1871871486381228" />
+          <meta name="og:title" content={title} />
           <title>{title}</title>
           <HTMLStyles />
           {this.props.headComponents}
