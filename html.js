ba81b4648836272d4a8784eceaeaeb3a6f08d3f2
abe_clark <abeaclark@gmail.com>
2017-01-10 15:21:18 -0800

    add some icons

--- html.js
+++ html.js
@@ -5,6 +5,19 @@ import typography from './utils/typography'
 import HTMLScripts from 'html-scripts'
 import HTMLStyles from 'html-styles'
 import Helmet from 'react-helmet'
+import appleicon57x57 from "media/logos/apple-icon-57x57.png"
+import appleicon60x60 from "media/logos/apple-icon-60x60.png"
+import appleicon72x72 from "media/logos/apple-icon-72x72.png"
+import appleicon76x76 from "media/logos/apple-icon-76x76.png"
+import appleicon114x114 from "media/logos/apple-icon-114x114.png"
+import appleicon120x120 from "media/logos/apple-icon-120x120.png"
+import appleicon144x144 from "media/logos/apple-icon-144x144.png"
+import appleicon152x152 from "media/logos/apple-icon-152x152.png"
+import appleicon180x180 from "media/logos/apple-icon-180x180.png"
+import androidicon192x192 from "media/logos/android-icon-192x192.png"
+import favicon32x32 from "media/logos/favicon-32x32.png"
+import favicon96x96 from "media/logos/favicon-96x96.png"
+import favicon16x16 from "media/logos/favicon-16x16.png"
 
 module.exports = React.createClass({
   displayName: 'HTML',
@@ -17,6 +30,20 @@ module.exports = React.createClass({
     return (
       <html lang="en">
         <head>
+          <link rel="apple-touch-icon" sizes="57x57" href={appleicon57x57} />
+          <link rel="apple-touch-icon" sizes="60x60" href={appleicon60x60} />
+          <link rel="apple-touch-icon" sizes="72x72" href={appleicon72x72} />
+          <link rel="apple-touch-icon" sizes="76x76" href={appleicon76x76} />
+          <link rel="apple-touch-icon" sizes="114x114" href={appleicon114x114} />
+          <link rel="apple-touch-icon" sizes="120x120" href={appleicon120x120} />
+          <link rel="apple-touch-icon" sizes="144x144" href={appleicon144x144} />
+          <link rel="apple-touch-icon" sizes="152x152" href={appleicon152x152} />
+          <link rel="apple-touch-icon" sizes="180x180" href={appleicon180x180} />
+          <link rel="icon" type="image/png" sizes="192x192"  href={androidicon192x192} />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
+          <link rel="icon" type="image/png" sizes="96x96" href={favicon96x96} />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
+
           <meta charSet="utf-8" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
